b3988ccc77a33c36feb21146535a1027d2d2c06e
sogobanwo <sogobanwo@gmail.com>
2022-12-25 04:29:12 +0100

    Desktop UI done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar";
+import Body from "./Components/Body";
+import Footer from "./Components/Footer";
+import "./Stylesheets/Desktop.css"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
